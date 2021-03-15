876548274ed2ab2adf3829bb820a6c7c077e007f
rhromets <rhromets@gmail.com>
2021-03-15 23:06:52 +0200

    User registration

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // @route GET api/posts/test
-// @desc Test posts rout
+// @desc Test posts route
 // @access Public
 router.get("/test", (req, res) => res.json({msg: "Posts works"}));
 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // @route GET api/profile/test
-// @desc Test profile rout
+// @desc Test profile route
 // @access Public
 router.get("/test", (req, res) => res.json({msg: "Profile works"}));
 
